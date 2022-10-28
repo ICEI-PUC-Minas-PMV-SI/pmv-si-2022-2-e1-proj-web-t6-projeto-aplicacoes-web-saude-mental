b35710430eb3cc2bca2a629c78208347829a9abd
Giovanni Marino <110241105+marinogv@users.noreply.github.com>
2022-10-28 17:43:43 -0300

    Add Banner

--- src/script.js
+++
@@ -1,7 +0,0 @@
-const menuHamburguer = document.querySelector(".menu-hamburguer");
-const navMenu = document.querySelector(".cabecalho-menu");
-
-menuHamburguer.addEventListener("click", () => {
-  menuHamburguer.classList.toggle("active");
-  navMenu.classList.toggle("active");
-});
