66f6385ca312dfc4586f91bf90271969415b0853
Giovanni Marino <110241105+marinogv@users.noreply.github.com>
2022-12-09 17:14:03 -0300

    Animacoes separadas

---
+++ src/animacoes.js
@@ -0,0 +1,31 @@
+// ANIMAÇÕES
+
+// 1 - Smooth scrooling
+
+const buttonScroll = document.querySelector("#button-scroll");
+const content01 = document.querySelector("#content01");
+
+buttonScroll.addEventListener("click", function (e) {
+  content01.scrollIntoView({ behavior: "smooth" });
+});
+
+// 2 - Reveal sections
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  entry.target.classList.remove("section-hidden");
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
+});
