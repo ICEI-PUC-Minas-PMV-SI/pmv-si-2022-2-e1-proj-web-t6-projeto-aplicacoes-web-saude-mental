7b7eca3dd60e1e285beeeb2f3727e26a57a2faa4
Giovanni Marino <110241105+marinogv@users.noreply.github.com>
2022-12-09 08:40:14 -0300

    Add smooth scrolling

--- src/script.js
+++ src/script.js
@@ -1,73 +1,90 @@
+// LOGIN
 
 window.onload = function () {
+  // Página de Cadastro e de Login não precisam dessa validação
+  if (
+    !window.location.href.endsWith("login.html") &&
+    !window.location.href.endsWith("cadastro.html")
+  ) {
+    const loginBtn = document.getElementById("login-button");
+    const usrm = sessionStorage.getItem("usrname");
 
-    // Página de Cadastro e de Login não precisam dessa validação
-    if ((!window.location.href.endsWith("login.html")) && (!window.location.href.endsWith("cadastro.html"))) {
-        const loginBtn = document.getElementById("login-button");
-        const usrm = sessionStorage.getItem('usrname');
-
-        if (usrm == null) { // Quer dizer que não foi feito o Login
-            loginBtn.innerHTML = "Login";
-            loginBtn.href = "login.html";
-        } else {
-            loginBtn.innerHTML = "Log Out";
-            loginBtn.onclick = function() {
-                sessionStorage.removeItem("usrname");
-                window.location.reload();
-
-            }
-        }
+    if (usrm == null) {
+      // Quer dizer que não foi feito o Login
+      loginBtn.innerHTML = "Login";
+      loginBtn.href = "login.html";
+    } else {
+      loginBtn.innerHTML = "Log Out";
+      loginBtn.onclick = function () {
+        sessionStorage.removeItem("usrname");
+        window.location.reload();
+      };
     }
+  }
 };
 
-
 function registrosDefaults() {
-    sessionStorage.setItem("registrados", JSON.stringify({
-            "henrique": "pass",
-            "alyson": "pass123",
-            "giovanni": "pass456",
-            "joao": "pass789",
-        })
-    );
+  sessionStorage.setItem(
+    "registrados",
+    JSON.stringify({
+      henrique: "pass",
+      alyson: "pass123",
+      giovanni: "pass456",
+      joao: "pass789",
+    })
+  );
 }
 
 function getRegistrados() {
-    var registrados = sessionStorage.getItem('registrados')
-    
-    if (registrados == null) {
-        registrosDefaults()
-    }
+  var registrados = sessionStorage.getItem("registrados");
 
-    return JSON.parse(sessionStorage.getItem('registrados'));
+  if (registrados == null) {
+    registrosDefaults();
+  }
+
+  return JSON.parse(sessionStorage.getItem("registrados"));
 }
 
 function loginSubmit() {
-    const usrname = document.getElementById("usrname").value;
-    const psw = document.getElementById("psw").value;
+  const usrname = document.getElementById("usrname").value;
+  const psw = document.getElementById("psw").value;
 
-    const registrados = getRegistrados();
+  const registrados = getRegistrados();
 
-    // Valida se o usuario e a senha estão corretos
-    if (registrados[usrname] == psw) {
-        sessionStorage.setItem("usrname", usrname);
-        window.location.href = 'index.html';
-    } else {
-        alert("Usuario ou Senha incorreto.");
-    }
+  // Valida se o usuario e a senha estão corretos
+  if (registrados[usrname] == psw) {
+    sessionStorage.setItem("usrname", usrname);
+    window.location.href = "index.html";
+  } else {
+    alert("Usuario ou Senha incorreto.");
+  }
 }
 
 function registrar() {
-    const usrname = document.getElementById("usrname").value;
-    const psw = document.getElementById("psw").value;
+  const usrname = document.getElementById("usrname").value;
+  const psw = document.getElementById("psw").value;
 
-    if ((usrname == "") || (psw == "")) {
-        alert("É necessario um usuario e senha para cadastro.");
-    } else {
-        var registrados = getRegistrados();
+  if (usrname == "" || psw == "") {
+    alert("É necessário um usuário e senha para cadastro.");
+  } else {
+    var registrados = getRegistrados();
 
-        registrados[usrname] = psw;
-        window.location.href = 'login.html';
-        
-        sessionStorage.setItem("registrados", JSON.stringify(registrados));
-    }
+    registrados[usrname] = psw;
+    window.location.href = "login.html";
+
+    sessionStorage.setItem("registrados", JSON.stringify(registrados));
+  }
 }
+
+// ANIMAÇÕES
+
+// 1 - Smooth scrooling
+
+const buttonScroll = document.querySelector("#button-scroll");
+const content01 = document.querySelector("#content01");
+
+buttonScroll.addEventListener("click", function (e) {
+  content01.scrollIntoView({ behavior: "smooth" });
+});
+
+// 2 - Reveal sections
